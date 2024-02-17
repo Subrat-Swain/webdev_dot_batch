9165b060c28fd51568c1550972369169beeb6864
Subrat-Swain <subratswain19991@gmail.com>
2024-02-17 15:28:35 +0530

    Js_Basics-I_intro_var_const_types_dynam_oper_equli_inc/dec_tern_preced_control_switch_loops

---
+++ 22_JS_BASICS-I/index.js
@@ -0,0 +1,78 @@
+// Line Below is used to print on the console window
+console.log('Namaste Dunia version 4');
+
+// let a = 5;
+// let a = 'love';
+// let a = true;
+// console.log(a);
+
+// const num = 12;
+// num = 13;
+
+// let lastName = 'Subrat';
+
+// console.log(lastName);
+
+// lastName = 3;
+
+// console.log(lastName);
+
+// console.log(3 > 3);
+
+// console.log(3 === 3); // Strict equality;
+
+// console.log(5 === 3);
+// console.log(5 !== 3); // not equality;
+
+// let age = 27;
+// let status = (age >= 18) ? 'Yes Vote' : 'No Vote';
+
+// console.log(status);
+
+
+// let marks = 88;
+// if(marks >= 90) {
+//     console.log('A');
+// }
+// else if(marks >= 80){
+//     console.log('B');
+// }
+// else if(marks >= 70){
+//     console.log('C');
+// }
+// else if(marks >= 60){
+//     console.log('D');
+// }
+// else{
+//     console.log('E');
+// }
+
+
+// let num = 2;
+
+// switch(num){
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3: console.log('C');
+//     break;
+//     default: console.log('D');
+// }
+
+for (let i = 1; i <= 5 ; i ++){
+    console.log('subrat ' + i);
+}
+
+let x = 1;
+
+while(x <= 5){
+    console.log(x);
+    x++;
+}
+
+let y = 1;
+do{
+    console.log(y);
+    y++;
+}while(y<10);
