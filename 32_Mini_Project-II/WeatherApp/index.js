1841a574ceb989ba33a67444b09a194a9626ac3e
Subrat-Swain <subratswain19991@gmail.com>
2024-05-16 17:13:21 +0530

    Weather_app_html_google_font_font_awesome_icon_dynamic_attribute_css_all_unset_object_fit_contain_n_cover_p_first_of_type_n_last_of_type_js_fetching_elements_set_initial_vairable_addEventListner_session_storage_sessionStorage_getItem_fetch_API_renderweather

--- 32_Mini_Project-II/WeatherApp/index.js
+++ 32_Mini_Project-II/WeatherApp/index.js
@@ -10,6 +10,7 @@ const userInfoContainer = document.querySelector(".user-info-container");
 // Initially variable needs??
 let currentTab = userTab;
 const API_KEY = "d8f335330984a63b79e2ff183596079e";
+// const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
 currentTab.classList.add("current-tab");
 
 // pending one task???
@@ -106,10 +107,10 @@ function renderWeatherInfo(weatherInfo){
     countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
     desc.innerText = weatherInfo?.weather?.[0]?.description;
     weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
-    temp.innerText = weatherInfo?.main?.temp;
-    windspeed.innerText = weatherInfo?.wind?.speed;
-    humidity.innerText = weatherInfo?.main?.humidity;
-    cloudiness.innerText = weatherInfo?.clouds?.all;
+    temp.innerText = `${weatherInfo?.main?.temp} °C`;
+    windspeed.innerText = `${weatherInfo?.wind?.speed} m/s`;
+    humidity.innerText = `${weatherInfo?.main?.humidity} %`;
+    cloudiness.innerText = `${weatherInfo?.clouds?.all} %`;
 }
 
 
@@ -143,11 +144,16 @@ grantAccessButton.addEventListener("click", getLocation);
 const searchInput = document.querySelector("[data-searchInput]");
 searchForm.addEventListener("submit",(e) => {
     e.preventDefault();
-    let cityName = searchInput.value
+    let cityName = searchInput.value;
     if(cityName === "")
-        return;
+        {
+            // some issue
+            loadingScreen.classList.remove("active");
+            userInfoContainer.classList.remove("active");
+            return;
+        }
     else
-        fetchSearchWeatherInfo(city);
+        fetchSearchWeatherInfo(cityName);
 });
 
 async function fetchSearchWeatherInfo(city){
@@ -157,11 +163,12 @@ async function fetchSearchWeatherInfo(city){
 
     try{
         const response = await fetch(
-            `https://api.openweathermap.org/data/2.5/wather?q=${city}&appid=${API_KEY}&units=metric`
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
         );
         const data = await response.json();
         loadingScreen.classList.remove("active");
         userInfoContainer.classList.add("active");
+        // console.log(data);
         renderWeatherInfo(data);
     }
     catch(err){
